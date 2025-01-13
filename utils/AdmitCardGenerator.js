8a211f775c306bed44ae560ac0f45c72667214c2
jatin34090 <jatin34090@gmail.com>
2025-01-13 14:29:55 +0530

    Add render-build.sh file

--- utils/AdmitCardGenerator.js
+++ utils/AdmitCardGenerator.js
@@ -26,27 +26,25 @@ const isFileValid = (filePath) => {
 
 const generateAdmitCardPDF = async (data, filePath) => {
 
-
-
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-      ],
-    });
   
 
-
   console.log("data form generateAdmitCardPDF", data);
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: '/usr/bin/google-chrome-stable', // Or another Chromium path
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
+  
+    const page = await browser.newPage();
   const logoPath = path.resolve(__dirname, 'SDATLogo.png');
   const logoBase64 = fs.readFileSync(logoPath, { encoding: 'base64' });
   const logoDataUrl = `data:image/png;base64,${logoBase64}`;
-
-  const htmlContent =
-    `
+  
+const htmlContent = 
+`
 <!DOCTYPE html>
 <html>
   <head>
@@ -166,7 +164,7 @@ const generateAdmitCardPDF = async (data, filePath) => {
             <div class="mainStream">
               <label> Medical </label>
               <input type="checkbox" id="medical" ${data.stream === 'Medical' ?
-      'checked' : ''}/>
+              'checked' : ''}/>
             </div>
 
             <div class="mainStream">
@@ -177,18 +175,18 @@ const generateAdmitCardPDF = async (data, filePath) => {
             <div class="mainStream">
               <label>Foundation</label>
               <input type="checkbox" id="foundation" ${data.stream ===
-      'Foundation' ? 'checked' : ''}/>
+              'Foundation' ? 'checked' : ''}/>
             </div>
           </div>
           <div class="class-section">
           <label>Class:</label>
-            ${['V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'].map(classLevel => `
+            ${['V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'] .map(classLevel=> `
               <div class="mainClass">
               <label
               >${classLevel}</label>
                <input type="checkbox"
               id="class-${classLevel.toLowerCase()}" ${data.class === classLevel
-          ? 'checked' : ''}/>
+              ? 'checked' : ''}/>
               </div>
               
               
@@ -274,29 +272,29 @@ const generateAdmitCardPDF = async (data, filePath) => {
 //             font-weight: bold;
 //             margin-bottom: 20px;
 //           }
-
+  
 //           .admit-card .stream-section,
 //           .admit-card .class-section {
 //             display: flex;
 //             gap: 20px;
 //             margin: 10px 0;
 //           }
-
+  
 //           .admit-card input[type="checkbox"] {
 //             margin-right: 5px;
 //           }
-
+  
 //           .admit-card .details-section {
 //             margin-top: 20px;
 //           }
-
+  
 //           .admit-card .details-section label {
 //             display: inline-block;
 //             width: 200px;
 //             font-weight: bold;
 //             margin-bottom: 10px;
 //           }
-
+  
 //           .admit-card .photo-section {
 //             border: 2px dashed white;
 //             width: 150px;
@@ -310,14 +308,14 @@ const generateAdmitCardPDF = async (data, filePath) => {
 //             color: white;
 //             text-align: center;
 //           }
-
+  
 //           .admit-card .footer {
 //             font-size: 12px;
 //             text-align: center;
 //             margin-top: 20px;
 //             font-weight: bold;
 //           }
-
+  
 //           .mainSection {
 //             width: 100%;
 //             display: flex;
@@ -361,7 +359,7 @@ const generateAdmitCardPDF = async (data, filePath) => {
 //                 <label>Center Name:</label>
 //                 <span>${data.CenterAddress}</span>
 //                 <br />
-
+               
 //               </div>
 //             </div>
 //             <div class="photo-section">
@@ -439,7 +437,7 @@ const processHTMLAndGenerateAdmitCards = async (student) => {
 
 
   };
-  console.log("studentData after the function", studentData);
+  console.log("studentData after the function",  studentData );
 
   try {
     // Choose between generating the PDF from HTML or PDFKit
